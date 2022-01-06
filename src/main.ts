fece6e8917010ec80ebf6fd010c1831024a858fc
Novanda Ahsan <novandaahsan1@gmail.com>
2022-01-06 11:38:17 +0700

    todo app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.mount("#app");
