de2f6ddb2ac0447794cd9ef6c8111af4d4f77c32
Novanda Ahsan <novandaahsan1@gmail.com>
2022-01-06 15:35:20 +0700

    initial commit

---
+++ store/todo.ts
@@ -0,0 +1,62 @@
+import create from "zustand";
+import Todo from "../src/components/Todo";
+
+type Todo = {
+  id: number;
+  task: string;
+  completed: boolean;
+};
+
+type TodoState = {
+  todos: Todo[];
+  nextId: number;
+  addTodo: (task: string) => void;
+  removeTodo: (todo: Todo) => void;
+  toggleCompleted: (todo: Todo) => void;
+};
+
+const useTodoStore = create<TodoState>((set) => ({
+  todos: [
+    {
+      id: 1,
+      task: "Wash shoes",
+      completed: true,
+    },
+    {
+      id: 2,
+      task: "Cook for dinner",
+      completed: false,
+    },
+    {
+      id: 3,
+      task: "Exercise react code",
+      completed: false,
+    },
+  ],
+  nextId: 4,
+  addTodo: (task: string) =>
+    set((state) => {
+      const todos = state.todos;
+      todos.unshift({ task, id: state.nextId++, completed: false });
+      return { todos };
+    }),
+  removeTodo: (todo: Todo) =>
+    set((state) => {
+      return { todos: state.todos.filter((td) => td !== todo) };
+    }),
+  toggleCompleted: (todo: Todo) =>
+    set((state) => {
+      const todos = state.todos;
+      const index = todos.findIndex((td) => td === todo);
+
+      const foundTodo = todos.find((td) => td === todo);
+      if (foundTodo) {
+        foundTodo.completed = !todo.completed;
+        todos[index] = foundTodo;
+      }
+
+      return { todos };
+    }),
+}));
+
+export default useTodoStore;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
