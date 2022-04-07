6b0564fb3bad7aaf59aa65377e1dd6a38b31d906
Novanda Ahsan <novandaahsan1@gmail.com>
2022-04-07 16:19:17 +0700

    initial setup react storybook

---
+++ react-storybook/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./stories/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
